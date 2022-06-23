ce3564537d45d42ab258008828cfe332a6e25d46
dlsdk0601 <dlsdk0601@gmail.com>
2022-06-24 02:51:06 +0900

    node login function success

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,9 +1,14 @@
 import db from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export interface IUserSchema {
+const slatRounds = 10;
+
+export interface IUserSchema extends db.Document {
   name?: string;
+  id: string;
   email?: string;
-  password?: string;
+  password: string;
   role?: number;
   token?: string;
   tokenExp?: number;
@@ -38,7 +43,29 @@ const userSchema = new db.Schema({
   },
 });
 
-const User = db.model("User", userSchema);
+// save라는 함수를 실행하기 직전 실행되는 함수
+userSchema.pre<IUserSchema>("save", async function (next) {
+  const user: IUserSchema = this;
+  const password = user.password || "";
+
+  // 정보가 변경되지 않았다면, 바로 next 실행
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  // salt 생성
+  const salt = await bcrypt.genSalt(slatRounds);
+
+  // 암호화된 비번 생성
+  const hashedPW = await bcrypt.hash(password, salt);
+  // 유저 인스턴스에 암호화된 비밀번호 대입
+  user.password = hashedPW;
+
+  // register callback 실행
+  next();
+});
+
+const User = db.model<IUserSchema>("User", userSchema);
 
 // 다른곳에서 쓸수 있게 모듈로 export
 export default User;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import db from "mongoose";
+import db, { AnyExpression } from "mongoose";
 import dotenv from "dotenv";
 import User, { IUserSchema } from "../models/User";
 import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // 환경변수
 dotenv.config();
@@ -16,6 +18,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // -- application/json 을 분석해서 가져옴
 app.use(bodyParser.json());
+
+const ERROROBJECT: { success: boolean; message: string } = {
+  success: false,
+  message: "통신에 실패했습니다.",
+};
 
 // 몽구스 옵션 타입
 type IMongooseOption = {
@@ -39,6 +46,7 @@ db.connect(
   .then(() => console.log("good"))
   .catch((err) => console.log(err));
 
+// register API
 app.post("/register", (req, res) => {
   // req.body를 사용할 수 잇는것은 body-parser 때문임
   const user = new User(req.body);
@@ -46,11 +54,79 @@ app.post("/register", (req, res) => {
   // 스키마를 만들고 model로 감쌌는데 결국 하나의 class를 만든것이고,
   // user는 우리가 만든 User의 인스턴스가 되는거임.
   // 때문에 서버통신에서 각 해당 값들 잘 넘겨줘야함.
-  user.save((err: string, userInfo: IUserSchema) => {
+  user.save((err: Error | null, userInfo: IUserSchema) => {
     if (err) return res.json({ success: false, err });
 
     return res.status(200).json({
       success: true,
+      userInfo,
+    });
+  });
+});
+
+// login API
+app.post("/login", async (req, res) => {
+  const {
+    body: { id, password },
+  } = req;
+
+  // 요청된 아이디을 db에서 찾기
+  const userInfo: IUserSchema | null = await User.findOne({ id });
+  // , async (err: Error | null, userInfo: IUserSchema) => {
+  // if (err) return res.json({ success: false, err });
+
+  // 유저 정보가 없다면
+  if (!userInfo) {
+    return res.json({
+      loginSuccess: false,
+      message: "아이디가 존재하지 않습니다.",
+    });
+  }
+
+  // 아이디가 db에 있으면 비밀번호 확인
+  const check = await bcrypt.compare(password, userInfo.password);
+
+  if (!check) {
+    return res.json({
+      loginSuccess: false,
+      message: "비밀번호가 틀렸습니다.",
+    });
+  }
+
+  // 비밀번호 맞으면 토근 생성
+  const { _id: userId } = userInfo;
+  const token = jwt.sign({ userId }, "secretToken");
+  userInfo.token = token;
+
+  const userInfoSave: IUserSchema | null = await userInfo.save();
+
+  if (!userInfoSave) {
+    return res.json({
+      loginSuccess: false,
+      message: "토큰 저장 실패",
+    });
+  }
+
+  return res.json({
+    loginSuccess: true,
+    message: "로그인 성공",
+    userInfo,
+  });
+});
+
+app.get("/select", (req, res) => {
+  const {
+    body: { id },
+  } = req;
+  User.findOne({ id }, async (err: Error | null, userInfo: IUserSchema) => {
+    if (err) {
+      return res.json({
+        message: "로그인 성공",
+      });
+    }
+    return res.json({
+      loginSuccess: true,
+      message: "로그인 성공",
       userInfo,
     });
   });
