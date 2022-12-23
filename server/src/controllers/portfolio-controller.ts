c48fe7e6a8c54de8074038b7b411a3a75b295212
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-23 21:39:25 +0900

    refactor: 불필요한 console 및 주석 제거

--- server/src/controllers/portfolio-controller.ts
+++ server/src/controllers/portfolio-controller.ts
@@ -5,15 +5,12 @@ import { resJsonType } from "../utils/resType";
 const createPortfolio = (req: Request, res: Response) => {
   const { body } = req;
 
-  // console.log("body==");
-  // console.log(body);
   const test = Buffer.from(body.base64, "base64").toString("utf8");
-  console.log("test===");
-  console.log(test);
 
   const resJson = resJsonType<null>(null, 200);
   return res.json(resJson);
 };
+
 // const updatePortfolio = () => {};
 // const deletePortfolio = () => {};
 // const selectPortfolio = () => {};
