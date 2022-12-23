be4462211c3e0e2618fddd4b2585bc74ffe66e15
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-23 21:40:52 +0900

    refactor: isAuth 미들웨어 추가

--- server/src/routes/auth/auth.ts
+++ server/src/routes/auth/auth.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { body } from "express-validator";
 import authController from "../../controllers/auth-controller";
+import isAuth from "../../middleware/is-auth";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", authController.register);
 authRouter.post("/login", authController.login);
-authRouter.get("/authification", authController.userData);
+authRouter.get("/user-info", isAuth, authController.userData);
 
 export { authRouter };
