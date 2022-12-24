455e8d6830f13360d48ecd7e479da832e489996c
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-24 14:08:02 +0900

    feat: 상수 값들 한곳에서 관리

---
+++ server/src/utils/constant.ts
@@ -0,0 +1,19 @@
+export const errorText = {
+  notFindUserId: "아이디가 존재하지 않습니다.",
+  wrongPassword: "비밀번호가 틀렸습니다.",
+  saveFailToken: "토큰 저장 실패",
+  notFindUserData: "유저 데이터를 찾지 못했습니다.",
+  notAuthenticated: "Not authenticated",
+  saveFailDB: "DB에 저장 실패",
+};
+
+export const errorCode = {
+  success: 200,
+  fail: 500,
+  notFindUserId: 601,
+  wrongPassword: 602,
+  saveFailToken: 603,
+  notFindUserData: 606,
+  notAuthenticated: 401,
+  saveFailDB: 600,
+};
