30bcadfec8598f251b4bbf47a9cecb997ee85364
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-24 14:08:41 +0900

    refactor: service 로직은 service 단에서 관리하게 수정

--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -4,28 +4,20 @@ import { resJsonType } from "../utils/resType";
 import authService from "../services/auth-service";
 
 // register
-const register = (req: Request, res: Response) => {
-  // req.body를 사용할 수 잇는것은 body-parser 때문임
-  const user = new User(req.body);
-
-  // 스키마를 만들고 model로 감쌌는데 결국 하나의 class를 만든것이고,
-  // user는 우리가 만든 User의 인스턴스가 되는거임.
-  // 때문에 서버통신에서 각 해당 값들 잘 넘겨줘야함.
-  user.save((err: Error | null, userInfo: IUserSchema) => {
-    if (err) {
-      const resJson = resJsonType<null>(null, 600, "DB에 유저 저장 실패", err);
-      return res.json(resJson);
-    }
-
-    const resJson = resJsonType<IUserSchema>(userInfo, 200);
-    return res.json(resJson);
-  });
+const register = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await authService.registerService(req);
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 };
 
 // login
 const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const data = authService.loginService(req.body, next);
+    const data = authService.loginService(req.body);
     return res.status(200).json(data);
   } catch (err: any) {
     console.log(err);
@@ -34,25 +26,14 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 // userData
-const userData = async (req: Request, res: Response) => {
-  const { headers } = req;
-
-  const token = headers?.authorization;
-
-  if (!token) {
-    const resJson = resJsonType<null>(null, 605, "토큰 정보가 잘못돼었습니다.");
-    return res.json(resJson);
+const userData = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await authService.getUserDataService(req);
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
-
-  const userData: IUserSchema | null = await User.findOne({ token });
-
-  if (!userData) {
-    const resJson = resJsonType<null>(null, 606, "해당 유저 정보가 없습니다.");
-    return res.json(resJson);
-  }
-
-  const resJson = resJsonType<IUserSchema>(userData, 200);
-  return res.json(resJson);
 };
 
 export default {
