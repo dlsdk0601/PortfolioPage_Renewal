7abf4a41489a2b7eb467cbc4772b428408462c9b
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-24 14:09:12 +0900

    refactor: 회원가입 유효성 검사 추가

--- server/src/routes/auth/auth.ts
+++ server/src/routes/auth/auth.ts
@@ -5,7 +5,16 @@ import isAuth from "../../middleware/is-auth";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", authController.register);
+authRouter.post(
+  "/register",
+  [
+    body("name").not().isEmpty(),
+    body("id").isLength({ max: 10 }).not().isEmpty(),
+    body("email").isEmail().normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+  ],
+  authController.register,
+);
 authRouter.post("/login", authController.login);
 authRouter.get("/user-info", isAuth, authController.userData);
 
