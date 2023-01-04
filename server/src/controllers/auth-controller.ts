230399a08f58322d5e18ab78842ceead9cb4a611
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-05 00:24:59 +0900

    refactor: 회원가입에서 catch문 수정

--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -1,15 +1,26 @@
 import { Response, Request, NextFunction } from "express";
+import { ValidationError } from "express-validator";
 import authService from "../services/auth-service";
 import { STATUS_CODE } from "../utils/constant";
+import { ICustomError } from "../utils/schema";
 
 // register
-const register = async (req: Request, res: Response, next: NextFunction) => {
+const register = async (req: Request, res: Response) => {
   try {
     const data = await authService.registerService(req);
     return res.status(STATUS_CODE.success).json(data);
-  } catch (err) {
-    console.log(err);
-    next(err);
+  } catch (error) {
+    // CHECK :: try catch에서 err 타입이 any에서 unknown으로 바껴서
+    // 타입 설정을 한번 걸쳐준다
+    const err = error as ICustomError;
+
+    const code = err.code ?? STATUS_CODE.fail;
+    const msg = err?.message ?? "";
+    const errorData = err?.data[0] ?? null;
+
+    return res
+      .status(STATUS_CODE.fail)
+      .json({ msg, data: null, code, result: false, error: errorData });
   }
 };
 
